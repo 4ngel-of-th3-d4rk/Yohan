7b745c2a72c3d98675538aa198038e345641ba05
4ngel-of-th3-d4rk <yoharroz@gmail.com>
2026-03-25 09:52:14 -0300

    Add shopping cart simulation project in Node.js
    Projeto desenvolvido em Node.js que simula o funcionamento de um carrinho de compras da Shopee, permitindo adicionar, remover produtos, calcular total e organizar itens.

---
+++ jornada_para_o_futuro/carrinho shoppe.js
@@ -0,0 +1,5 @@
+https://github.com/4ngel-of-th3-d4rk/Yohan/new/main/jornada_para_o_futuro
+
+Projeto desenvolvido em Node.js que simula o funcionamento de um carrinho de compras da Shopee. O sistema permite adicionar produtos ao carrinho, remover itens,
+  calcular o valor total da compra e organizar os itens selecionados pelo usuário. O projeto utiliza conceitos de modularização, exportação de módulos (CommonJS/ESM),
+  funções assíncronas e boas práticas de organização de código em Node.js.
